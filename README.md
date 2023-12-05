b2f88a45ae9c1b7fff934b852f97adac9fc93e2f
Hum Ler <hum.ler@outlook.com>
2023-12-05 19:15:21 +0800

    Add command line arguments to run specific day / part

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ Practise solving [Advent of Code 2023](https://adventofcode.com/2023) using Rust
 
 Quick hack -- input is never checked, unwrap() everywhere -- code will panic from any error.
 
-[`regex`](https://github.com/rust-lang/regex) crate is used to parse input.
+To run the code against the examples, use `cargo test --test run_example`
 
-To run the code against the examples, use `cargo test -- --test run_example`
-To run the code against the input data, use `cargo run`
+To run the code against the input data, use `cargo run --release <day> <part>`
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use clap::Parser;
+
 mod day_1_part_1;
 mod day_1_part_2;
 mod day_2_part_1;
@@ -9,37 +11,27 @@ mod day_4_part_2;
 mod day_5_part_1;
 mod day_5_part_2;
 
+#[derive(Parser)]
+struct Args {
+    day: u8,
+    part: u8,
+}
+
 fn main() {
-    {
-        let day_1_input = input("input/day-1.txt");
-        println!("-- Day 1 --");
-        println!(" Part 1: {}", day_1_part_1::run(&day_1_input));
-        println!(" Part 2: {}", day_1_part_2::run(&day_1_input));
-    }
-    {
-        let day_2_input = input("input/day-2.txt");
-        println!("-- Day 2 --");
-        println!(" Part 1: {}", day_2_part_1::run(&day_2_input));
-        println!(" Part 2: {}", day_2_part_2::run(&day_2_input));
-    }
-    {
-        let day_3_input = input("input/day-3.txt");
-        println!("-- Day 3 --");
-        println!(" Part 1: {}", day_3_part_1::run(&day_3_input));
-        println!(" Part 2: {}", day_3_part_2::run(&day_3_input));
-    }
-    {
-        let day_4_input = input("input/day-4.txt");
-        println!("-- Day 4 --");
-        println!(" Part 1: {}", day_4_part_1::run(&day_4_input));
-        println!(" Part 1: {}", day_4_part_2::run(&day_4_input));
-    }
-    {
-        let day_5_input = input("input/day-5.txt");
-        println!("-- Day 5 --");
-        println!(" Part 1: {}", day_5_part_1::run(&day_5_input));
-        println!(" Part 2: {}", day_5_part_2::run(&day_5_input));
-    }
+    let args = Args::parse();
+    match (args.day, args.part) {
+        (1, 1) => println!("{}", day_1_part_1::run(&input("input/day-1.txt"))),
+        (1, 2) => println!("{}", day_1_part_2::run(&input("input/day-1.txt"))),
+        (2, 1) => println!("{}", day_2_part_1::run(&input("input/day-2.txt"))),
+        (2, 2) => println!("{}", day_2_part_2::run(&input("input/day-2.txt"))),
+        (3, 1) => println!("{}", day_3_part_1::run(&input("input/day-3.txt"))),
+        (3, 2) => println!("{}", day_3_part_2::run(&input("input/day-3.txt"))),
+        (4, 1) => println!("{}", day_4_part_1::run(&input("input/day-4.txt"))),
+        (4, 2) => println!("{}", day_4_part_2::run(&input("input/day-4.txt"))),
+        (5, 1) => println!("{}", day_5_part_1::run(&input("input/day-5.txt"))),
+        (5, 2) => println!("{}", day_5_part_2::run(&input("input/day-5.txt"))),
+        _ => (),
+    };
 }
 
 pub fn input<P>(path: P) -> String
