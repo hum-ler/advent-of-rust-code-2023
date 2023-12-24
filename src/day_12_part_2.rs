5df48de8e1410818dc693a7c91913b5120ade764
Hum Ler <hum.ler@outlook.com>
2023-12-24 22:48:36 +0800

    Optimize solution for day 12 part 1

--- src/day_12_part_2.rs
+++ src/day_12_part_2.rs
@@ -1,59 +1,50 @@
-use crate::day_12_part_1::*;
+use rayon::prelude::*;
 
-pub fn run(_input: &str) -> usize {
-    todo!()
+use crate::{clean_lines, day_12_part_1::*};
+
+pub fn run(input: &str) -> usize {
+    let parsed_input = clean_lines(input)
+        .map(parse_line)
+        .collect::<Vec<(Vec<char>, Vec<usize>)>>();
+
+    parsed_input
+        .par_iter()
+        .map(|(pattern, counts)| arrangements(pattern, counts))
+        .sum()
 }
 
-fn _arrangements(_pattern: &[Spring], _count: &[usize]) -> usize {
-    todo!()
-}
-
-/// Partially match and consume any Spring::Any plus its count.
-///
-/// Return None if the pattern fails the matching. Otherwise, Some((pattern_left, count_left)) for
-/// further matching.
-fn _is_partial_match(_pattern: &[Spring], _count: &[usize]) -> Option<(Vec<Spring>, Vec<usize>)> {
-    todo!()
-}
-
-fn _parse_line(input: &str) -> (Vec<Spring>, Vec<usize>) {
+fn parse_line(input: &str) -> (Vec<char>, Vec<usize>) {
     let (pattern_part, counts_part) = input.split_once(' ').unwrap();
 
-    let pattern = pattern_part
-        .chars()
-        .map(Spring::from)
-        .collect::<Vec<Spring>>();
-    let mut unfolded_pattern = vec![];
-    unfolded_pattern.extend_from_slice(&pattern);
-    unfolded_pattern.push(Spring::Any);
-    unfolded_pattern.extend_from_slice(&pattern);
-    unfolded_pattern.push(Spring::Any);
-    unfolded_pattern.extend_from_slice(&pattern);
-    unfolded_pattern.push(Spring::Any);
-    unfolded_pattern.extend_from_slice(&pattern);
-    unfolded_pattern.push(Spring::Any);
-    unfolded_pattern.extend_from_slice(&pattern);
+    let pattern = pattern_part.chars().collect::<Vec<char>>();
+    let mut pattern_extended = pattern.clone();
+    pattern_extended.push('?');
+    pattern_extended.extend(&pattern);
+    pattern_extended.push('?');
+    pattern_extended.extend(&pattern);
+    pattern_extended.push('?');
+    pattern_extended.extend(&pattern);
+    pattern_extended.push('?');
+    pattern_extended.extend(&pattern);
 
     let counts = counts_part
         .split(',')
         .map(str::parse::<usize>)
         .map(Result::unwrap)
         .collect::<Vec<usize>>();
-    let mut unfolded_counts = vec![];
-    unfolded_counts.extend_from_slice(&counts);
-    unfolded_counts.extend_from_slice(&counts);
-    unfolded_counts.extend_from_slice(&counts);
-    unfolded_counts.extend_from_slice(&counts);
-    unfolded_counts.extend_from_slice(&counts);
+    let mut counts_extended = counts.clone();
+    counts_extended.extend(&counts);
+    counts_extended.extend(&counts);
+    counts_extended.extend(&counts);
+    counts_extended.extend(&counts);
 
-    (unfolded_pattern, unfolded_counts)
+    (pattern_extended, counts_extended)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[ignore]
     #[test]
     fn run_example() {
         let input = r"
